bfcb89b38668fa7c7dab45e434f0b9025ae3d913
Tyler Woods <tyler.woods66@gmail.com>
2016-11-18 16:19:39 -0600

    initial commit

---
+++ SoftwareDeploy.ps1
@@ -0,0 +1,11 @@
+﻿# Install software on IP range remotely with PSExec
+# and CMD.exe for launching a non-interactive installer
+#
+
+$iprange = Read-Host 'What is the IP Range of computers to install software on? (Example: If your subnet is 192.168.100.XXX, enter 192.168.100.)'
+
+for($i=2; $i -le 254; $i++)
+    {
+        .\psexec.exe -c \\"$iprange$i" SOFTWARE.exe
+        
+    }
